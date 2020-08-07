c71a77776b7cc81b317a483bcdb3caf47334aebb
Yanuar Aditia <yanuaraditia@outlook.com>
2020-08-07 13:14:13 +0700

    Update

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <b-navbar>
+        <template slot="brand">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <img
+                    src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+                    alt="Lightweight UI components for Vue.js based on Bulma"
+                >
+            </b-navbar-item>
+        </template>
+        <template slot="end">
+            <b-navbar-item href="#">
+                Home
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                Documentation
+            </b-navbar-item>
+            <b-navbar-dropdown label="Info">
+                <b-navbar-item href="#">
+                    About
+                </b-navbar-item>
+                <b-navbar-item href="#">
+                    Contact
+                </b-navbar-item>
+            </b-navbar-dropdown>
+            <b-navbar-item tag="div">
+                <div class="buttons">
+                    <g-link
+                    to="/join"
+                    class="button is-rounded is-outlined has-text-primary has-text-weight-bold"
+                    >Get a Quote</g-link>
+                </div>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+</template>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import Buefy from 'buefy'
+import infiniteScroll from 'vue-infinite-scroll'
+import '~/code/main.scss'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+
+  // Use buefy
+  Vue.use(Buefy)
+  Vue.use(infiniteScroll)
+  Vue.config.productionTip  = false
 }
